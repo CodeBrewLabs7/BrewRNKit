be28dcb3f46df0d258d6a7ec1c26cc929c6b6c98
gulsher7 <gulsherkhan0005@gmail.com>
2024-03-04 02:35:00 +0530

    feat: added template

---
+++ template.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    placeholderName: 'BrewRNKit',
+    titlePlaceholder: 'BrewRNKit React Native Boilerplate',
+    templateDir: './template',
+    postInitScript: './post-init.script.js',
+};
