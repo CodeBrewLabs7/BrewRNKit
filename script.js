47938d4018fca92a7145324bf0a57ceb7a943d7e
gulsher7 <gulsherkhan0005@gmail.com>
2024-03-05 16:24:50 +0530

    chore: changes in script.js file

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 #!/usr/bin/env node
 const ora = require('ora');
+const { exec } = require('child_process');
 
 const spinner = ora('Executing post init script ');
 
-new Promise((resolve) => {
-  spinner.start();
-  // do something
-  resolve();
+new Promise((resolve, reject) => {
+    spinner.start();
+    
+    // Install pods
+    const installPods = exec('cd ios && pod install');
+    installPods.stdout.on('data', data => console.log(data));
+    installPods.stderr.on('data', data => console.error(data));
+    installPods.on('close', code => {
+        if (code === 0) {
+            resolve();
+        } else {
+            reject();
+        }
+    });
 }).then(() => {
-  spinner.succeed();
+    spinner.succeed();
 }).catch(() => {
-  spinner.fail();
-  throw new Error('Something went wrong during the post init script execution');
+    spinner.fail();
+    throw new Error('Something went wrong during the post init script execution');
 });
