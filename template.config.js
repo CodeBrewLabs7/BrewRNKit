91a247f36cd7a79c9eac64bd786cbd6d168fd39a
gulsher7 <gulsherkhan0005@gmail.com>
2024-03-05 16:51:26 +0530

    chore: change post-init.script file

--- template.config.js
+++ template.config.js
@@ -2,5 +2,5 @@ module.exports = {
     placeholderName: 'BrewRNKit',
     titlePlaceholder: 'BrewRNKit React Native Boilerplate',
     templateDir: './template',
-    postInitScript: './script.js',
+    postInitScript: './post-init.script.js',
 };
