016fbe9c3db359bb021dab923a020a02ffb49519
gulsher7 <gulsherkhan0005@gmail.com>
2024-03-09 16:57:32 +0530

    chore: removed server dir

--- template/server/src/config/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-main().then(res=> console.log("db connected successfully..!!"))
-main().catch(err => console.log("db not connected...!!!", err));
-
-
-async function main() {
-  await mongoose.connect(process.env.DB);
-}
