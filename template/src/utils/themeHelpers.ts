0458cee1cf1cd52f102a91dd2d8de10ff8e032ff
gulsher7 <gulsherkhan0005@gmail.com>
2024-03-09 18:54:15 +0530

    chore: add UnistylesRuntime method

--- template/src/utils/themeHelpers.ts
+++ template/src/utils/themeHelpers.ts
@@ -1,10 +1,8 @@
-import { ThemeMode } from "src/typings/global";
-import { Appearance } from "react-native";
-import { setItem } from "src/services/apiService";
 import { UnistylesRuntime } from "react-native-unistyles";
+import { setItem } from "src/services/apiService";
+import { ThemeMode } from "src/typings/global";
 
 export const changeTheme = (mode: ThemeMode) => {
   setItem("defaultTheme", {myTheme: mode});
   UnistylesRuntime.setTheme(mode)
-  // Appearance.setColorScheme(mode);
 };
